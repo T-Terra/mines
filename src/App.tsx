a29f431259731c85cb490adb06a62dc66f1fdfa3
T-Terra <gabst138@gmail.com>
2024-09-22 18:02:25 -0300

    feat: end the project

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,24 @@ import {
   StyleSheet,
   Text,
   View,
+  Alert,
 } from 'react-native';
 
 import params from './Params';
 import Field from './components/Field';
 import MineField from './components/MineField';
-import { createMineBoard } from './Logic';
+import Header from './components/Header';
+import LevelSelection from './screens/LevelSelection';
+import { 
+  createMineBoard,
+  clonedBoard,
+  openField,
+  hadExplosion,
+  wonGame,
+  showMines,
+  invertFlag,
+  flagsUsed,
+} from './Logic';
 
 export default class App extends Component  {
 
@@ -28,19 +40,60 @@ export default class App extends Component  {
     const cols = params.getColumnsAmount()
     const rows = params.getRowsAmount()
     return {
-      board: createMineBoard(rows, cols, this.minesAmount())
+      board: createMineBoard(rows, cols, this.minesAmount()),
+      won: false,
+      lost: false,
+      showLevelSelection: false,
     }
+  }
+
+  onOpenField = (row, column) => {
+    const board = clonedBoard(this.state.board)
+    openField(board, row, column)
+    const lost = hadExplosion(board)
+    const won = wonGame(board)
+
+    if(lost) {
+      showMines(board)
+      Alert.alert('Perdeeeu!')
+    }
+
+    if(won) {
+      Alert.alert('Parabéns', 'Você Venceu!')
+    }
+
+    this.setState({ board, lost, won })
+  }
+
+  onSelectField = (row, column) => {
+    const board = clonedBoard(this.state.board)
+    invertFlag(board, row, column)
+    const won = wonGame(board)
+
+    if(won) {
+      Alert.alert('Parabéns', 'Você ganhou!!!')
+    }
+
+    this.setState({ board, won })
+  }
+
+  onLevelSelected = level => {
+    params.difficultLevel = level
+    this.setState(this.createState())
   }
 
   render(): React.ReactNode {
     return (
-      <SafeAreaView style={styles.sectionContainer}>
-        <Text style={styles.sectionTitle}>Iniciando o Mines!</Text>
-        <Text style={styles.highlight}>Tomanho da grade: 
-          {params.getRowsAmount()}X{params.getColumnsAmount()}
-        </Text>
+      <SafeAreaView style={styles.container}>
+        <LevelSelection isVisible={this.state.showLevelSelection}
+          onLevelSelected={this.onLevelSelected}
+          onCancel={() => this.setState({ showLevelSelection: false })} />
+        <Header flagsLeft={this.minesAmount() - flagsUsed(this.state.board)} onNewGame={() => this.setState(this.createState())} onFlagPress={() => this.setState({ showLevelSelection: true })} />
         <View style={styles.board}>
-          <MineField board={this.state.board}/>
+          <MineField board={this.state.board}
+            onOpenField={this.onOpenField}
+            onSelectField={this.onSelectField}
+          />
         </View>
       </SafeAreaView>
     );
